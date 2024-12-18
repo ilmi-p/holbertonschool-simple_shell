029b73a085b72fc8996e1c2b8323d48fe7b28c81
ilmi-p <iveliu96@gmail.com>
2024-12-18 08:41:58 +0000

    test 1 by ilmi for Betty would be proud

---
+++ test_betty.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints "test" to the console
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("test");
+return (0);
+}
