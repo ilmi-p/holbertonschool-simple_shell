3c796656fa9950542ff5ee14e3092549347d3af5
ilmi-p <iveliu96@gmail.com>
2025-01-07 09:25:09 +0000

    ajout for funtion for the 001

--- main.h
+++ main.h
@@ -1,11 +1,20 @@
-#ifndef SHELL
-#define SHELL
+#ifndef MAIN_H
+#define MAIN_H
 
+/* bibliothèques nécessaires */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdarg.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/* Déclaration des fnc pour le 1*/
+char *_strcat(char *first, char *second);
+int _strlen(char *str);
+int _strcmp(char *s1, char *s2);
+char *_strchr(char *str, char c);
+char *_strdup(char *str);
 
 #endif
