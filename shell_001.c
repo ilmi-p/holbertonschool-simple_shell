1509890a45bc765d8fca072e2fe001b0e634323f
ilmi-p <iveliu96@gmail.com>
2025-01-07 13:25:30 +0000

    tes 001 5

--- shell_001.c
+++ shell_001.c
@@ -1,49 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+#include "simple_shell.h"
 
 /**
- * main - Minimal UNIX shell
+ * main - Entry point for the shell
  * @argc: Number of arguments (unused)
  * @argv: Array of arguments
- * Return: 0 on success, or exit status on failure
+ * @envp: Environment variables
+ * Return: 0 on success, or error code on failure
  */
-int main(int argc, char **argv)
+int main(int argc, char **argv, char **envp)
 {
-    char *command_line = NULL;
-    size_t bufsize = 0;
-    ssize_t nread;
-    char *args[2];
+	char *command_line = NULL;
+	size_t bufsize = 0;
+	ssize_t nread;
+	char *args[2];
 
-    (void)argc;
+	(void)argc;
 
-    while (1)
-    {
-        printf("#cisfun$ ");
-        fflush(stdout);
+	while (1)
+	{
+		write(STDOUT_FILENO, "#cisfun$ ", 9);
 
-        nread = getline(&command_line, &bufsize, stdin);
-        if (nread == -1)
-        {
-            free(command_line);
-            putchar('\n');
-            exit(0);
-        }
+		nread = getline(&command_line, &bufsize, stdin);
+		if (nread == -1)
+		{
+			free(command_line);
+			write(STDOUT_FILENO, "\n", 1);
+			exit(0);
+		}
 
-        command_line[strcspn(command_line, "\n")] = '\0';
-        if (command_line[0] == '\0')
-            continue;
+		command_line[strcspn(command_line, "\n")] = '\0';
 
-        args[0] = command_line;
-        args[1] = NULL;
+		if (command_line[0] == '\0')
+			continue;
 
-        if (execve(args[0], args, NULL) == -1)
-        {
-            perror(argv[0]);
-        }
-    }
+		args[0] = command_line;
+		args[1] = NULL;
 
-    free(command_line);
-    return (0);
+		if (execve(args[0], args, envp) == -1)
+			perror(argv[0]);
+	}
+
+	free(command_line);
+	return (0);
 }
