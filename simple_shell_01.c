fb9d081f6d63e9966fb8b9f22b768dab7f50288c
JESTER-7 <arthur.urbano33@gmail.com>
2025-01-09 10:19:50 +0100

    add the non-interactive mode

--- simple_shell_01.c
+++ simple_shell_01.c
@@ -10,59 +10,48 @@
 
 extern char **environ;
 
+/**
+ * main - basic unix shell function
+ * Return: 0
+ */
 int main(void)
 {
-    char input[MAX_INPUT];
-    char *args[2];
-    pid_t pid;
-    int status;
+	char max_input[MAX_INPUT];
+	char *args[2];
+	pid_t pid;
+	int stock;
 
-    while (1)
-    {
-        /* Display the prompt */
-        write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
-
-        /* Read user input */
-        if (fgets(input, MAX_INPUT, stdin) == NULL)
-        {
-            /* Handle EOF (Ctrl+D) */
-            write(STDOUT_FILENO, "\n", 1);
-            break;
-        }
-
-        /* Remove the newline character */
-        input[strcspn(input, "\n")] = '\0';
-
-        /* Check if the input is empty */
-        if (strlen(input) == 0)
-            continue;
-
-        /* Prepare arguments for execve */
-        args[0] = input;
-        args[1] = NULL;
-
-        /* Fork a child process */
-        pid = fork();
-        if (pid == -1)
-        {
-            perror("Error");
-            continue;
-        }
-        if (pid == 0)
-        {
-            /* In child process, execute the command */
-            if (execve(args[0], args, environ) == -1)
-            {
-                perror("./shell");
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            /* In parent process, wait for the child to finish */
-            waitpid(pid, &status, 0);
-        }
-    }
-
-    return 0;
+	while (1)
+	{
+		if (isatty(STDIN_FILENO)) /* Display the prompt for the non-interactive mode */
+			write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
+		if (fgets(max_input, MAX_INPUT, stdin) == NULL) /* Display the "shell" for the non-interactive mode */
+		{
+			if (isatty(STDIN_FILENO)) /* Handle Ctrl+D */
+				write(STDOUT_FILENO, "\n", 1);
+			break;
+		}
+		max_input[strcspn(max_input, "\n")] = '\0'; /* Remove \n */
+		if (strlen(max_input) == 0) /* Check if the input is empty */
+			continue;
+		args[0] = max_input; /* Arguments for execve */
+		args[1] = NULL;
+		pid = fork(); /* Child fork process */
+		if (pid == -1)
+		{
+			perror("Error");
+			continue;
+		}
+		if (pid == 0)
+		{
+			if (execve(args[0], args, environ) == -1) /* Exe the command in the child process */
+			{
+				perror("./shell");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+			waitpid(pid, &stock, 0); /* Wait for the child to finish */
+	}
+	return (0);
 }
