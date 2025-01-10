fc6e2a438e051f41a92471c1f57a7cc0aa625eba
JESTER-7 <arthur.urbano33@gmail.com>
2025-01-10 03:53:04 +0100

    add commentary to the command.c

--- command.c
+++ command.c
@@ -6,36 +6,36 @@
  */
 int user_command(char *command)
 {
-	char *args[2];
-	char *token;
-	pid_t pid;
-	int stock;
+	char *args[2]; /* array with the command and arguments */
+	char *token; /* pointer to split the string with the commands */
+	pid_t pid; /* pid of the child process */
+	int stock; /* statut of the child process */
 
-	token = strtok(command, " \n");
-	if (token == NULL)
+	token = strtok(command, " \n"); /* split the string accordind to space or \n */
+	if (token == NULL) /* check if any command was write */
 	{
 		fprintf(stderr, "Error: no command enterred\n");
 		return (1);
 	}
-	pid = fork();
-	if (pid < 0)
+	pid = fork(); /* create a new process */
+	if (pid < 0) /* if fork fail */
 	{
 		perror("error \n");
 		return (1);
 	}
-	else if (pid == 0)
+	else if (pid == 0) /* child process block */
 	{
-		args[0] = token;
+		args[0] = token; /* the first argument is the command */
 		args[1] = NULL;
-		if (execve(args[0], args, environ) == -1)
+		if (execve(args[0], args, environ) == -1) /* command, arguments and env variables */
 		{
 			perror("./hsh");
-			exit(EXIT_FAILURE);
+			exit(EXIT_FAILURE); /* exit the child process if execve fail */
 		}
 	}
-	else
+	else /* parents process */
 	{
-		if (waitpid(pid, &stock, 0) == -1)
+		if (waitpid(pid, &stock, 0) == -1) /* wait for the child process to finish and take the statut (stock) */
 		{
 			perror("Error: waitpid failed");
 			return (1);
